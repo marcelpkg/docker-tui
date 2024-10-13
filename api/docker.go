d6d76a31f99115c361357fbfacc8659d8a0c7278
marcelpkg <marcel.serdak@gmail.com>
2024-10-13 21:42:47 +0100

    Docker package additions

--- api/docker.go
+++ api/docker.go
@@ -1,16 +1,51 @@
 package docker
 
-import "fmt"
+import (
+	"context"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"log"
+)
 
-type container struct {
-	image string
-	id    string
+type Docker struct {
+	Cli        *client.Client
+	Containers []tuiContainer
 }
 
-func Init() {
-	fmt.Println("Hi")
+type tuiContainer struct {
+	Image   string
+	ID      string
+	Created int64
+	Status  string
+	State   string
 }
 
-func GetDockerContainers() []container {
-	return []container{}
+func InitDocker() Docker {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return Docker{
+		Cli:        cli,
+		Containers: []tuiContainer{},
+	}
+}
+
+func (d Docker) GetDockerContainers() {
+	containers, err := d.Cli.ContainerList(context.Background(), container.ListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var containerList []tuiContainer
+	for _, element := range containers {
+		containerList = append(containerList, tuiContainer{
+			Image:   element.Image,
+			ID:      element.ID,
+			Created: element.Created,
+			Status:  element.Status,
+			State:   element.State,
+		})
+
+	}
 }
